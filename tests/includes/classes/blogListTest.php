f6dedf8aa7a422e5bca3a25508e4bfc61eeb6534
Jess <j.j.isaacs@hotmail.co.uk>
2016-09-29 22:08:50 +0100

    add construct tests

--- tests/includes/classes/blogListTest.php
+++ tests/includes/classes/blogListTest.php
@@ -1,12 +1,25 @@
 <?php
 
-require ('../../../includes/classes/blog_list.php');
+require ('../../../includes/classes/blogList.php');
 
 class blogListTest extends PHPUnit_Framework_TestCase {
 
-//    public function testConstructGood(){}
+    /*
+     * testing that a new instance of the class is created
+     */
+    public function testConstructGood(){
+        $blogObject = new blogList('blogTest.php');
+        $this->assertInstanceOf(blogList::class, $blogObject);
+    }
 
-//    public function testConstructMal(){}
+    /*
+     * testing that when a passed a string that isn't a file path is entered,
+     * it returns an empty array
+     */
+    public function testConstructBad(){
+        $blogObject = new blogList('not blog list');
+        $this->assertEmpty($blogObject->blog_list);
+    }
 
 //    public function testGetListGood(){}
 
