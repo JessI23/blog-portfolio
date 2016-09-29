1411df8f4c6a34f8e9c4c6614f23594e7ee554e7
Jess <j.j.isaacs@hotmail.co.uk>
2016-09-29 15:01:42 +0100

    amended to enable testing

--- tests/includes/classes/blogTest.php
+++ tests/includes/classes/blogTest.php
@@ -4,9 +4,32 @@ require ('../../../includes/classes/blog.php');
 
 class blogTest extends PHPUnit_Framework_TestCase {
 
-//    public function testConstructGood(){}
+    /*
+     * testing that a new instance of the class is created
+     */
+    public function testConstructGood(){
+        $blog = new blog('blogListTest.php');
+        $this->assertInstanceOf(blog::class, $blog);
+    }
 
-//    public function testConstructMal(){}
+    /**
+     * testing that when a string that isn't a file path is entered, it...
+     *
+     * @expectedException PHPUnit_Framework_Error
+     */
+    public function testConstructBad(){
+        $blog = new blog('blog');
+
+    }
+
+    /*
+     * testing that when a file path isn't entered, it throws an error
+     */
+    public function testConstructMal(){
+        $array = [];
+        $blog = new blog($array);
+
+    }
 
 //    public function testGetTitleGood(){}
 
