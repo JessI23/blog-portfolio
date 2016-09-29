35dd4d58169cd411f10028394115e644fdf061a6
Jess <j.j.isaacs@hotmail.co.uk>
2016-09-29 19:21:55 +0100

    done good tests

--- tests/includes/classes/blogTest.php
+++ tests/includes/classes/blogTest.php
@@ -4,6 +4,13 @@ require ('../../../includes/classes/blog.php');
 
 class blogTest extends PHPUnit_Framework_TestCase {
 
+//    public function testThing() {
+//        $stub = $this->createMock(blog::class);
+//        $stub->method('getBlogContents')->willReturn('foo');
+//
+//        $this->assertEquals('foo', $stub->getBlogContents('fghj'));
+//    }
+
     /*
      * testing that a new instance of the class is created
      */
@@ -12,7 +19,7 @@ class blogTest extends PHPUnit_Framework_TestCase {
         $this->assertInstanceOf(blog::class, $blog);
     }
 
-    /**
+    /*
      * testing that when a passed a string that isn't a file path is entered,
      * it returns an empty array
      */
@@ -21,15 +28,61 @@ class blogTest extends PHPUnit_Framework_TestCase {
         $this->assertEmpty($blogObject->blog);
     }
 
-//    public function testGetTitleGood(){}
+    /*
+     * checks that function returns title of object
+     */
+    public function testGetTitleGood(){
+
+        $stub = $this->getMockBuilder(blog::class)
+            ->setMethods(array('getBlogContents'))
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('getBlogContents')->will($this->returnValue("title\n\nauthor\n\ndate\n\ncontents"));
+        $stub->__construct('blogListTest.php');
+
+        $return = $stub->getTitle();
+        $this->assertEquals($return, 'title');
+
+
+    }
 
 //    public function testGetTitleBad(){}
 
-//    public function testGetSubtitleGood(){}
+    public function testGetSubtitleGood(){
+
+        $stub = $this->getMockBuilder(blog::class)
+            ->setMethods(array('getBlogContents'))
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('getBlogContents')->will($this->returnValue("title\n\nauthor\n\ndate\n\ncontents"));
+        $stub->__construct('blogListTest.php');
+
+        $return = $stub->getSubtitle();
+        $this->assertEquals($return, 'by author<br>posted on: date');
+
+    }
 
 //    public function testGetSubtitleBad(){}
 
-//    public function testGetContentGood(){}
+    public function testGetContentGood(){
+
+        $stub = $this->getMockBuilder(blog::class)
+            ->setMethods(array('getBlogContents'))
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $stub->expects($this->any())
+            ->method('getBlogContents')->will($this->returnValue("title\n\nauthor\n\ndate\n\ncontents"));
+        $stub->__construct('blogListTest.php');
+
+        $return = $stub->getContent();
+        $this->assertEquals($return, 'contents');
+
+    }
 
 //    public function testGetContentBad(){}
 
