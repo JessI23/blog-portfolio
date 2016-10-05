27a3b75b5e054c9d0f810fa796d59d09db490271
Jess <j.j.isaacs@hotmail.co.uk>
2016-10-05 22:30:12 +0100

    added img to blog previews

--- blog.php
+++ blog.php
@@ -58,7 +58,7 @@
 
 <?php
 	function getBlogs($conn){
-		$sql = "SELECT `id`, `title`, `author`, `date_created`, `desc`, `slug` FROM `articles`;";
+		$sql = "SELECT `id`, `img`, `title`, `author`, `date_created`, `desc`, `slug` FROM `articles`;";
 		$query=$conn->query($sql);
 		return $query->fetchAll(PDO::FETCH_ASSOC);
 	}
@@ -66,6 +66,7 @@
 	function printList($blog_list){
 		foreach($blog_list as $key => $val){
 			$print[$key] = 	'<div class="section">' .
+								'<img src="' . $val['img'] . '" alt="blog logo">' .
 								'<h3>' . $val["title"] . '</h3>' .
 								'<p>' . $val["author"] . ' ' . $val["date_created"] . '</p>' .
 								'<p>' . $val["desc"] . '<a href="' . $val["slug"] . '">read more...</a></p>' .
